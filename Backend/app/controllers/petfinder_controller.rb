0690e02fc1fa244790f750dcca110e6ac31c1f6d
e-nk <enocklagat747@gmail.com>
2023-02-27 15:04:23 +0300

    backend controller update

---
+++ Backend/app/controllers/petfinder_controller.rb
@@ -0,0 +1,78 @@
+class TodoController < Sinatra::Base
+
+    set :views, './app/views'
+
+
+    get '/hello' do
+        "Welcome to my first controller"
+    end
+
+    post '/pets/create' do
+        data = JSON.parse (request.body.read)
+        #rescue block
+        begin
+        #approach 1 (individual columns)
+        # title = data ["title"]
+        # description =data["description"]
+        # todo = Todo.create(title: title, description: description, createdAt: today)
+        # todo.to_json
+
+        #approach 2 (hash of columns)
+        today = Time.now
+        data["createdAt"] = today
+        todo = Todo.create(data)
+        [201, todo.to_json]
+
+
+            
+        rescue => exception
+            [422, {
+                error: exception.message
+            }.to_json]
+        end
+    end
+
+    get '/pets' do
+
+        todos = Todo.all
+        [200, todos.to_json]
+
+    end
+
+    get '/view/pets' do
+        @todos = Todo.all
+        erb :todos
+
+    end
+
+    put 'pets/update/:id' do
+        #rescue bloc
+        begin
+        data = JSON.parse (request.body.read)
+        todo_id = params['id'].to_i
+        todo = Todo.find(todo_id)
+        todo.update(data)
+        {message: "Pet updated successfully"}.to_json 
+        rescue => e
+            {
+                error: e.message
+            }.to_json
+        end
+
+    end
+
+    delete '/pets/destroy/:id' do
+        begin
+            todo_id = params['id'].to_i
+            todo = Todo.find(todo_id)
+            todo.destroy
+            rescue => e
+                {error: e.message}.to_json
+
+            end
+
+    end
+
+
+
+end
