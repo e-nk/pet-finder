aae2adeb556ba0696aac3fcb329717b927ca2930
e-nk <enocklagat747@gmail.com>
2023-03-06 01:06:15 +0300

    navbar added

---
+++ Frontend/src/components/navigation/Navbar.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FaBars, FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
+import { links, social } from "./data"
+
+const Navbar = () => {
+const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <h3 className="logo">Petfinder</h3>
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div class="navbar bg-primary" data-bs-theme="dark">
+        {/* <div class="btn btn-dark" data-netlify-identity-menu></div> */}
+        <div class="btn btn-light" data-netlify-identity-button>Login/Signup: </div> 
+        </div> 
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
