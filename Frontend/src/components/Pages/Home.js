3881ae3938223695cfc7e120089aa2ca28c16937
e-nk <enocklagat747@gmail.com>
2023-03-06 12:11:08 +0300

    frontend update

--- Frontend/src/components/Pages/Home.js
+++ Frontend/src/components/Pages/Home.js
@@ -26,7 +26,7 @@ function Home() {
       </div>
         </div>
         <div class="ilustration">
-            <img src="https://images.pexels.com/photos/10875180/pexels-photo-10875180.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image here"></img>
+            <img src="https://images.pexels.com/photos/10875180/pexels-photo-10875180.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
         </div>
       </section>
     </main>
