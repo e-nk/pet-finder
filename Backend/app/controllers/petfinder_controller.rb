238fb91538a00af07ac694a2f3905055b55f4583
e-nk <enocklagat747@gmail.com>
2023-03-07 08:33:27 +0300

    Backend updated

--- Backend/app/controllers/petfinder_controller.rb
+++ Backend/app/controllers/petfinder_controller.rb
@@ -1,78 +1,95 @@
-class PetController < Sinatra::Base
+class PetController < AppController
 
     set :views, './app/views'
 
-
+    # @method: Display a small welcome message
     get '/hello' do
-        "Welcome to my first controller"
+        "Our very first controller"
     end
 
+    # @method: Add a new TO-DO to the DB
     post '/pets/create' do
-        data = JSON.parse (request.body.read)
-        #rescue block
         begin
-        #approach 1 (individual columns)
-        # title = data ["title"]
-        # description =data["description"]
-        # todo = Todo.create(title: title, description: description, createdAt: today)
-        # todo.to_json
-
-        #approach 2 (hash of columns)
-        today = Time.now
-        data["createdAt"] = today
-        pet = Pet.create(data)
-        [201, pet.to_json]
-
-
-            
-        rescue => exception
-            [422, {
-                error: exception.message
-            }.to_json]
-        end
-    end
-
-    get '/pets' do
-
-        pets = Pet.all
-        [200, pets.to_json]
-
-    end
-
-    get '/view/pets' do
-        @pets = Pet.all
-        erb :pets
-
-    end
-
-    put 'pets/update/:id' do
-        #rescue bloc
-        begin
-        data = JSON.parse (request.body.read)
-        pet_id = params['id'].to_i
-        pet = Pet.find(pet_id)
-        pet.update(data)
-        {message: "Pet updated successfully"}.to_json 
+            pet = Pet.create( self.data(create: true) )
+            json_response(code: 201, data: pet)
         rescue => e
-            {
-                error: e.message
-            }.to_json
+            json_response(code: 422, data: { error: e.message })
         end
-
     end
 
+    # @method: Display all todos
+    get '/pets' do
+        pets = Pet.all
+        json_response(data: pets)
+    end
+
+    # @view: Renders an erb file which shows all TODOs
+    # erb has content_type because we want to override the default set above
+    get '/' do
+        @pets = Pet.all.map { |pet|
+          {
+            pet: pet,
+            badge: pet_status_badge(pet.status)
+          }
+        }
+        @i = 1
+        erb_response :pets
+    end
+
+    # @method: Update existing TO-DO according to :id
+    put '/pets/update/:id' do
+        begin
+            pet = Pet.find(self.pet_id)
+            pet.update(self.data)
+            json_response(data: { message: "pet updated successfully" })
+        rescue => e
+            json_response(code: 422 ,data: { error: e.message })
+        end
+    end
+
+    # @method: Delete TO-DO based on :id
     delete '/pets/destroy/:id' do
         begin
-            pet_id = params['id'].to_i
-            pet = Pet.find(pet_id)
+            pet = Pet.find(self.pet_id)
             pet.destroy
-            rescue => e
-                {error: e.message}.to_json
-
-            end
-
+            json_response(data: { message: "pet deleted successfully" })
+        rescue => e
+          json_response(code: 422, data: { error: e.message })
+        end
     end
 
+
+    private
+
+    # @helper: format body data
+    def data(create: false)
+        payload = JSON.parse(request.body.read)
+        if create
+            payload["createdAt"] = Time.now
+        end
+        payload
+    end
+
+    # @helper: retrieve to-do :id
+    def pet_id
+        params['id'].to_i
+    end
+
+    # @helper: format status style
+    def pet_status_badge(status)
+        case status
+            when 'CREATED'
+                'bg-info'
+            when 'ONGOING'
+                'bg-success'
+            when 'CANCELLED'
+                'bg-primary'
+            when 'COMPLETED'
+                'bg-warning'
+            else
+                'bg-dark'
+        end
+    end
 
 
 end
